9ef2b50cfad992bb7f64c1d62b722a002e1cf9b4
mabi <crust87@gmail.com>
2019-01-18 10:23:04 +0900

    프로젝트 설정 변경

--- app/src/main/java/com/crust87/imagecroppersample/MainActivity.java
+++ app/src/main/java/com/crust87/imagecroppersample/MainActivity.java
@@ -5,7 +5,6 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.Menu;
